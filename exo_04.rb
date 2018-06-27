f83c2bbcbb21068c41736e05f624a7ecc33d980a
Romain SLR <37241638+RomainSai@users.noreply.github.com>
2018-06-27 21:13:39 +0200

    Update exo_04.rb

--- exo_04.rb
+++ exo_04.rb
@@ -1,4 +1,4 @@
 puts "salut, ça farte ?
 
-lorsque lance le programme un message d'erreur apparait "unterminated string meets end of file", 
+#lorsque lance le programme un message d'erreur apparait "unterminated string meets end of file", 
 car il manque la parenthese de fin après le "?".
