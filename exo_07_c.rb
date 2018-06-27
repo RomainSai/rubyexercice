d7d561ad1bb069e75547b0e12ce78c68d5157bcf
Romain SLR <37241638+RomainSai@users.noreply.github.com>
2018-06-27 21:20:17 +0200

    Create exo_07_c.rb

---
+++ exo_07_c.rb
@@ -0,0 +1,8 @@
+user_name = gets.chomp
+puts user_name
+
+#il existe plusieurs différences entre les trois programmes
+#le premier et le secon affiche une question "Bonjour, c'est quoi ton blase ?" le second affiche un ">" a 
+l'endroit ou l'utilisateur doit renseigner son nom alors que le troisieme ne pose pas de question, 
+l'utilisateur doit renseigner son nom directement. le point commun est l'affichage du nom à la fin du programme 
+via "puts user_name
