ba229cda88e22effa88da99ba04cb9fbdb81eed7
Romain SLR <37241638+RomainSai@users.noreply.github.com>
2018-06-27 21:12:22 +0200

    Update exo_02.rb

--- exo_02.rb
+++ exo_02.rb
@@ -1,4 +1,4 @@
 puts "Bonjour, monde !"
 puts "Et avec une voix sexy, ça donne : Bonjour, monde !"
 
-la différence entre les deux est que Puts ajoute une nouvelle ligne après l'exécution et le print ne le fait pas.
+#la différence entre les deux est que Puts ajoute une nouvelle ligne après l'exécution et le print ne le fait pas.
