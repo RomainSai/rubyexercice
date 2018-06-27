ec5038fa281298ae87423ee58f59481350cd7b4a
Romain SLR <37241638+RomainSai@users.noreply.github.com>
2018-06-27 21:12:59 +0200

    Update exo_03.rb

--- exo_03.rb
+++ exo_03.rb
@@ -1,4 +1,4 @@
 puts "Bonjour, monde !"
 #puts "Et avec une voix sexy, ça donne : Bonjour, monde !"
 
-la ligne se grise et elle n'est plus active
+#la ligne se grise et elle n'est plus active, il s'agit d'un commentaire
